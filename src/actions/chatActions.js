6e651f8999b721af2267ea2df777c622eec6fa93
Pavel Svitek <svitek.pavel@gmail.com>
2016-12-27 22:57:48 +0100

    refactored chatAddMessage to support delay

--- src/actions/chatActions.js
+++ src/actions/chatActions.js
@@ -1,35 +1,34 @@
 import * as types from '../constants/actionTypes';
 
-export function chatAddMessage(message) {
+export function chatAddMessage(message, delayed=false) {
   return function (dispatch) {
     return new Promise((resolve, reject) => {
       console.log('Adding new message:');
       console.log(message);
-      const words = message.message.split(' ').length;
+      const wordCount = message.message.split(' ').length;
 
       const pmsg = {
         author: message.author,
         message: '...',
       };
 
-      dispatch({
-          type: types.CHAT_ADD_MESSAGE,
-          payload: {
-            message: pmsg,
-          },
-        });
+      // Delayed message to illustrate user behavior
+      if (delayed) {
+        dispatch({ type: types.CHAT_ADD_MESSAGE, payload: { message: pmsg } });
 
-      setTimeout(() => {
+        setTimeout(() => {
+          dispatch({ type: types.CHAT_REPLACE_LAST_MESSAGE, payload: { message: message } });
+          resolve();
+        }, 800+((Math.random()+0.2)*(wordCount*500)));
+
+      // Display message instantly
+      } else {
         dispatch({
-          type: types.CHAT_REPLACE_LAST_MESSAGE,
-          // type: types.CHAT_ADD_MESSAGE,
-          payload: {
-            message: message,
-          },
+          type: types.CHAT_ADD_MESSAGE,
+          payload: { message: message },
         });
-
         resolve();
-      }, 800+((Math.random()+0.2)*(words*500)));
+      }
     });
   };
 }
