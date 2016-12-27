baba48340ae8d28251e90b5e1fe5c98202463484
Pavel Svitek <svitek.pavel@gmail.com>
2016-12-27 15:53:22 +0100

    removed fuelSave section from initialState.js

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,18 +1,38 @@
 export default {
-  fuelSavings: {
-    newMpg: '',
-    tradeMpg: '',
-    newPpg: '',
-    tradePpg: '',
-    milesDriven: '',
-    milesDrivenTimeframe: 'week',
-    displayResults: false,
-    dateModified: null,
-    necessaryDataIsProvidedToCalculateSavings: false,
-    savings: {
-      monthly: 0,
-      annual: 0,
-      threeYear: 0
-    }
-  }
+  chat: {
+    messages: [
+      {
+        author: 'Pavel',
+        message: 'Hi there, my name is Pavel',
+      },
+      {
+        author: 'Visitor',
+        message: 'Hi, my name is John',
+      },
+      {
+        author: 'Pavel',
+        message: 'Nice to meet you.. what brought you here?',
+      },
+      {
+        author: 'Visitor',
+        message: '...',
+      },
+      {
+        author: 'Pavel',
+        message: 'What are you interested in?',
+        loading: false,
+      },
+    ],
+    options: [
+      {
+        title: 'Javascript/ReactJS development',
+      },
+      {
+        title: 'Python development',
+      },
+      {
+        title: 'What do you read?',
+      },
+    ],
+  },
 };
